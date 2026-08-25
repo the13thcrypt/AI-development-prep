d0a661bb9a2e341965cf0b1ed599686379ce082b
Sameer Dahiya <sam0dahiya13@gmail.com>
2026-08-26 03:04:15 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employeedemographics
+ (EmpID int , firstname varchar (40) , lastname varchar(40) , age int , gender varchar (40));
+
+---CREATE TABLE employeesalary(
+---EmpID int,jobtitle varchar , salary int) 
+INSERT INTO employeedemographics VALUES (1001,'sam','dahiya',19,'male')
